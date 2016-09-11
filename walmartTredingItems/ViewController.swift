99229994f67e7e5f75db585c2b27d2fef9bb3f5b
John Regner <regnerjr@gmail.com>
2016-09-11 13:34:19 -0700

    Initial Commit

---
+++ walmartTredingItems/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  walmartTredingItems
+//
+//  Created by John Regner on 9/11/16.
+//  Copyright © 2016 WigglingScholars. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
